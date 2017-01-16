e92297df8f5512496869d6ae207b21b67fed3b5b
Roman <kaboom.ua@gmail.com>
2017-01-16 10:48:42 +0200

    Update randomer.js

--- Middlewares/randomer.js
+++ Middlewares/randomer.js
@@ -3,7 +3,7 @@ import { COMMENT_ADDED } from '../Constants';
 export default store => next => action => {
 
     const { type } = action;
-
+    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - плохая практика
     switch (type) {
         case COMMENT_ADDED:
             action.payload.id = (( min, max ) => Math.random() * (max - min) + min)(1, 100000);
